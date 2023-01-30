486ab25d9284e302ea9526d666b0ce06a12f1b1a
Yadnik Bendale <66904635+Yadnik1@users.noreply.github.com>
2023-01-30 17:51:24 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,149 +1,211 @@
-#include "FLASH_PAGE_F1.h"
+
+
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 #include "string.h"
 #include "stdio.h"
 
-#define FLASH_USER_START_ADDR  (FLASH_BASE + (4 * FLASH_PAGE_SIZE))  /* Start @ of user Flash area */
-#define FLASH_USER_END_ADDR   (FLASH_BASE + FLASH_SIZE - 1)   /* End @ of user Flash area */
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "FLASH_PAGE_F1.h"
+/* USER CODE END Includes */
 
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-uint32_t FirstPage=0, NbOfPages=0;
-uint32_t Address=0, PageError=0;
+/* USER CODE END PTD */
 
-__IO uint32_t data32=0 , MemoryProgramStatus=0;
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
 
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
 
-static uint32_t GetPage(uint32_t Address);
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint32_t Flash_Address=0x0800F800;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+char *data = "hello FLASH from ControllerTech\
+			  This is a test to see how many words can we work with";
+/* USER CODE END 0 */
 
 
 /**
-  * @brief  Gets the page of a given address
-  * @param  Addr: Address of the FLASH Memory
-  * @retval The page of a given address
+  * @brief  The application entry point.
+  * @retval int
   */
-
-static uint32_t GetPage(uint32_t Addr)
+int main(void)
 {
-  return (Addr - FLASH_BASE) / FLASH_PAGE_SIZE;;
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  int numofwords = (strlen(data)/4)+((strlen(data)%4)!=0);
+  printf("%d",numofwords);
+  Flash_Write_Data(0x0800F800 , (uint32_t *)data, numofwords);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
 }
 
-uint8_t bytes_temp[4];
-
-
-void float2Bytes(uint8_t * ftoa_bytes_temp,float float_variable)
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-    union {
-      float a;
-      uint8_t bytes[4];
-    } thing;
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    thing.a = float_variable;
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    for (uint8_t i = 0; i < 4; i++) {
-      ftoa_bytes_temp[i] = thing.bytes[i];
-    }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PD2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 }
 
-float Bytes2float(uint8_t * ftoa_bytes_temp)
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
 {
-    union {
-      float a;
-      uint8_t bytes[4];
-    } thing;
-
-    for (uint8_t i = 0; i < 4; i++) {
-    	thing.bytes[i] = ftoa_bytes_temp[i];
-    }
-
-   float float_variable =  thing.a;
-   return float_variable;
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
-uint32_t Flash_Write_Data( uint32_t Address , uint32_t *Data, uint16_t numberofwords)
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
 {
-	Address = FLASH_USER_START_ADDR;
-	static FLASH_EraseInitTypeDef EraseInitStruct;
-	uint32_t PAGEError;
-	int sofar=0;
-
-	HAL_FLASH_Unlock();
-
-  /* Get the 1st page to erase */
-  FirstPage = GetPage(FLASH_USER_START_ADDR);
-
-  /* Get the number of pages to erase from 1st page */
-  NbOfPages = GetPage(FLASH_USER_END_ADDR) - FirstPage + 1;
-
-  /* Fill EraseInit structure*/
-  EraseInitStruct.TypeErase   = FLASH_TYPEERASE_PAGES;
-  EraseInitStruct.Page        = FirstPage + numberofwords*4;
-  EraseInitStruct.NbPages     = NbOfPages;
-
- if (HAL_FLASHEx_Erase(&EraseInitStruct, &PAGEError) != HAL_OK)
-	   {
-	     /*Error occurred while page erase.*/
-		  return HAL_FLASH_GetError ();
-	   }
-
-while (sofar<numberofwords)
-	   {
-	     if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, Address, Data[sofar]) == HAL_OK)
-	     {
-	    	 Address += 8;  // use StartPageAddress += 2 for half word and 8 for double word
-	    	 sofar++;
-	     }
-	     else
-	     {
-	       /* Error occurred while writing data in Flash memory*/
-	    	 return HAL_FLASH_GetError ();
-	     }
-
-	}
-
-	  /* Lock the Flash to disable the flash control register access (recommended
-	      to protect the FLASH memory against possible unwanted operation) *********/
-	   HAL_FLASH_Lock();
-
-	   return 0;
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
-
-void Flash_Read_Data( uint32_t Address, uint32_t *RxBuf, uint16_t numberofwords)
-{
-	while (1)
-	{
-
-		*RxBuf = *(__IO uint32_t *)Address;
-		Address += 4;
-		RxBuf++;
-		if (!(numberofwords--)) break;
-	}
-}
-void Convert_To_Str (uint32_t *Data, char *Buf)
-{
-	int numberofbytes = ((strlen((char *)Data)/4) + ((strlen((char *)Data) % 4) != 0)) *4;
-
-	for (int i=0; i<numberofbytes; i++)
-	{
-		Buf[i] = Data[i/4]>>(8*(i%4));
-	}
-}
-
-
-void Flash_Write_NUM (uint32_t StartSectorAddress, float Num)
-{
-
-	float2Bytes(bytes_temp, Num);
-
-	Flash_Write_Data (StartSectorAddress, (uint32_t *)bytes_temp, 1);
-}
-
-
-float Flash_Read_NUM (uint32_t StartSectorAddress)
-{
-	uint8_t buffer[4];
-	float value;
-
-	Flash_Read_Data(StartSectorAddress, (uint32_t *)buffer, 1);
-	value = Bytes2float(buffer);
-	return value;
-}
+#endif /* USE_FULL_ASSERT */
